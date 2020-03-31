5132262dd8ac874ba2af6604d2014e8285ad4a0d
atdaunais <atdaunais@gmail.com>
2020-03-31 11:29:34 -0500

    first commit. initiated project, installed MtgApiManager library

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MtgApiManager.Lib.Service;
+using MtgApiManager.Lib.Core;
+using MtgApiManager.Lib.Dto;
+
+namespace MTGCardOrganizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardService service = new CardService();
+            var result = service.All();
+            System.Console.WriteLine(result.ToString());
+
+            // CardService service = new CardService();
+            // Exceptional<List<Card>> result = service.All();
+            // if (result.IsSuccess)
+            // {
+            //     var value = result.Value;
+            // }
+            // else
+            // {
+            //     var exception = result.Exception;
+            // }
+        }
+    }
+}
